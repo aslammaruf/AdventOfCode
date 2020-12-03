7cd16671ab450c936bafa7589b0a1fc60f13621c
Aslam Maruf <42076284+aslammaruf@users.noreply.github.com>
2020-12-03 11:49:13 +0000

    Day03 Complete

---
+++ 2020/Day03/Day03.py
@@ -0,0 +1,26 @@
+import math
+
+def TobogganTrajectory( arr, rowIncrement, colIncrement ):
+    treeEncounter = row = col = 0
+    rowBound = len( arr ) 
+    colBound = len( arr[0] )
+
+    while row < rowBound:
+        if arr[ row ][ col ] == "#":
+            treeEncounter += 1
+
+        row += rowIncrement
+        col += colIncrement 
+
+        if col >= colBound:
+            col = col - colBound
+
+    return treeEncounter
+
+with open('Day03/input.txt') as f:
+    arr = [ list(line.strip()) for line in f ]
+
+print( f'Part 1 result : { TobogganTrajectory( arr , 1 , 3 ) }') 
+
+slopeList = [[1,1], [1,3], [1,5], [1,7], [2,1]]
+print( f'Part 2 result : { math.prod([ TobogganTrajectory( arr, down , right ) for down, right in slopeList ]) }')
